ae4168d9774d2730ad4c24220f7e028dc2a0ece2
SergioBerteli <sergiobbf2002@gmail.com>
2023-10-22 23:53:36 -0300

    perfis e criação de perfil

---
+++ app/src/main/java/com/example/t3_mobile/CriaPerfilActivity.kt
@@ -0,0 +1,16 @@
+package com.example.t3_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.t3_mobile.databinding.ActivityCriaPerfilBinding
+
+class CriaPerfilActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCriaPerfilBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCriaPerfilBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
